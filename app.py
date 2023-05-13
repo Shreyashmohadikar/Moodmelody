22ea9439edbfde0dd6fc1a40516544e0d00aa9aa
Shreyash <shreyashmohadikar@gmail.com>
2023-05-13 21:02:50 +0530

    new

--- app.py
+++ app.py
@@ -9,6 +9,11 @@ from flask import Flask, request, render_template
 from werkzeug.utils import secure_filename
 import statistics as st
 
+gpus = tf.config.experimental.list_physical_devices('GPU')
+for gpu in gpus:
+    tf.config.experimental.set_memory_growth(gpu, True)
+
+
 app = Flask(__name__)
 
 
