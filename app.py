ac010fd6fb5ad1ae4b70866d8b5d1d5f535a05a8
Shreyash <75601500+Shreyashmohadikar@users.noreply.github.com>
2023-05-13 21:44:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,57 +24,58 @@ def home():
 
 @app.route('/capture', methods=['GET', 'POST'])
 def camera():
-    GR_dict = {0: (0, 255, 0), 1: (0, 0, 255)}
+    try:
+        GR_dict = {0: (0, 255, 0), 1: (0, 0, 255)}
 
+        model = tf.keras.models.load_model('final_model.h5')
+        face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-    model = tf.keras.models.load_model('final_model.h5')
-    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+        output = []
+        cap = cv2.VideoCapture(0)
 
-    output = []
-    cap = cv2.VideoCapture(0)
+        i = 0
+        while i <= 30:
+            ret, img = cap.read()
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            faces = face_cascade.detectMultiScale(gray, 1.05, 5)
 
-    i = 0
-    while (i <= 30):
-        ret, img = cap.read()
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        faces = face_cascade.detectMultiScale(gray, 1.05, 5)
+            for x, y, w, h in faces:
+                face_img = img[y:y+h, x:x+w]
+                resized = cv2.resize(face_img, (224, 224))
+                reshaped = resized.reshape(1, 224, 224, 3)/255
+                predictions = model.predict(reshaped)
 
-        for x, y, w, h in faces:
+                # find max indexed array
+                max_index = np.argmax(predictions[0])
 
-            face_img = img[y:y+h, x:x+w]
+                emotions = ('angry', 'disgust', 'fear', 'happy', 'sad', 'neutral', 'surprise')
+                predicted_emotion = emotions[max_index]
+                output.append(predicted_emotion)
 
-            resized = cv2.resize(face_img, (224, 224))
-            reshaped = resized.reshape(1, 224, 224, 3)/255
-            predictions = model.predict(reshaped)
+                cv2.rectangle(img, (x, y), (x+w, y+h), GR_dict[1], 2)
+                cv2.rectangle(img, (x, y-40), (x+w, y), GR_dict[1], -1)
+                cv2.putText(img, predicted_emotion, (x, y-10),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 255, 255), 2)
+            i = i+1
 
-            # find max indexed array
-            max_index = np.argmax(predictions[0])
+            cv2.imshow('LIVE', img)
+            key = cv2.waitKey(1)
+            if key == 10:
+                cap.release()
+                cv2.destroyAllWindows()
+                break
 
-            emotions = ('angry', 'disgust', 'fear', 'happy',
-                        'sad', 'neutral', 'surprise')
-            predicted_emotion = emotions[max_index]
-            output.append(predicted_emotion)
+        print(output)
+        cap.release()
+        cv2.destroyAllWindows()
 
-            cv2.rectangle(img, (x, y), (x+w, y+h), GR_dict[1], 2)
-            cv2.rectangle(img, (x, y-40), (x+w, y), GR_dict[1], -1)
-            cv2.putText(img, predicted_emotion, (x, y-10),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 255, 255), 2)
-        i = i+1
+        final_output1 = st.mode(output)
+        return render_template("capture.html", final_output=final_output1)
 
-        cv2.imshow('LIVE', img)
-        key = cv2.waitKey(1)
-        if key == 10:
-            cap.release()
-            cv2.destroyAllWindows()
-            break
-    print(output)
-    cap.release()
-    cv2.destroyAllWindows()
-
-    final_output1 = st.mode(output)
-    final_output1
-    return render_template("capture.html",final_output=final_output1)
-
+    except Exception as e:
+        return "An error occurred: " + str(e)
+    
+    
 @app.route('/templates/capture_emotion', methods = ['GET','POST'])
 def capture():
     return render_template("capture.html")
